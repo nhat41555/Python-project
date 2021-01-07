ccacfe61f79fff83b06cdb0ca593b0bc79ecdf9a
lenhat41555 <batmantrongtinhyeu123@gmail.com>
2021-01-07 09:10:42 +0700

    update

---
+++ timmax.py
@@ -0,0 +1,10 @@
+import random
+
+n = int(input('Nhap n :'))
+list = random.sample(range(10,99),n)
+max = list[0]
+for item in list:
+    if item > max :
+        max = item
+print('List = ',list)
+print('Max la :',max)
